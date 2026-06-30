ff88fbc354fbb8c014424381b4d5cb8531ef9554
Deepanshu Upadhyay <deepanshuupadhyay603@gmail.com>
2026-06-30 13:47:14 +0530

    Time: 0 ms (100%), Space: 8.5 MB (87.06%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x<=0) return x;
+        else{
+            double x0=x;
+            double x1= (x0 +x/x0)/2;
+            while((x0-x1)>0){
+                x0=x1;
+                x1=(x0 +x/x0)/2;
+            }
+            return (int)x1;
+        }
+    }
+};
