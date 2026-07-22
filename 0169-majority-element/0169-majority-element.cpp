b50f8c8a0ee1461f276894de98cf9225f023a0d0
Deepanshu Upadhyay <deepanshuupadhyay603@gmail.com>
2026-07-22 17:16:32 +0530

    Time: 0 ms (100%), Space: 28.2 MB (36.45%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int max=nums[0];
+        int count=0;
+        for(int i=0;i<nums.size();i++){
+            if(count==0) max=nums[i];
+
+            if(nums[i]==max) count++;
+            else count--;
+        }
+        return max;
+    }
+};
