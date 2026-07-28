7275445ddbf8f681117d69531cdde1ac4c926d84
Deepanshu Upadhyay <deepanshuupadhyay603@gmail.com>
2026-07-28 11:46:18 +0530

    Time: 13 ms (96.26%), Space: 54.2 MB (88.13%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
-    // bool palindrome(string s){
-    //     for(int i=0;i<s.size()/2;i++){
-    //         if(s[i]==)
-    //     }
-    // }
     string smallestPalindrome(string s) {
         int n=s.size();
-        if(n<2) return s;
-        sort(s.begin(),s.begin()+n/2);
+        int f[26]={};
+        for(int i=0;i<n/2;i++){
+            f[s[i]-'a']++;
+        }
+        int idx=0;
+        for(int c=0;c<26;c++){
+            while(f[c]--){
+                s[idx++]=char(c+'a');
+            }
+        }
         for(int i=0;i<n/2;i++){
             s[n-1-i]=s[i];
         }
