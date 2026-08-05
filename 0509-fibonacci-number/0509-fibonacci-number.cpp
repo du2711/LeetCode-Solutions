defe3a9747f60a1b0f219d5fc850449cbd492b59
Deepanshu Upadhyay <deepanshuupadhyay603@gmail.com>
2026-08-05 15:13:45 +0530

    Time: 3 ms (64.78%), Space: 7.7 MB (65.72%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,7 +1,14 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n<=1) return n;
-        else return fib(n-1)+fib(n-2);
+        if(n<=0) return n;
+        int a=0,b=1;
+
+        for(int i=2;i<=n;i++){
+            int curr=a+b;
+            a=b;
+            b=curr;
+        }
+        return b;
     }
 };
