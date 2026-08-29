cc226666a9b50626459abb692901a82e81e28b0c
Deepanshu Upadhyay <deepanshuupadhyay603@gmail.com>
2026-08-29 15:37:47 +0530

    Time: 2114 ms (6.16%), Space: 7.8 MB (21.36%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.cpp
+++ 0507-perfect-number/0507-perfect-number.cpp
@@ -1,13 +1,9 @@
 class Solution {
 public:
     bool checkPerfectNumber(int num) {
-        vector<int> lcms;
-        for(int i=1;i<num;i++){
-            if(num%i==0) lcms.push_back(i);
-        }
         int sum=0;
-        for(int i=0;i<lcms.size();i++){
-            sum+=lcms[i];
+        for(int i=1;i<num;i++){
+            if(num%i==0) sum+=i;
         }
         return num==sum;
     }
